dcdb744f8fc6735b76d5380fda6cbf8d39b4ef15
bjrk99 <37029865+bjrk99@users.noreply.github.com>
2019-12-22 21:36:56 +0000

    Added extension reload command

--- extensions/core.py
+++ extensions/core.py
@@ -10,5 +10,15 @@ class Core(commands.Cog):
 	async def ping(self, ctx):
 		await ctx.send("Pong!")
 
+	@commands.command()
+	async def reload(self, ctx, extention):
+		await ctx.bot.unload_extention(f"extensions.{extention}")
+		
+		try:
+			await ctx.bot.load(f"extensions.{extention}")
+		except:
+			await ctx.send("Error loading extension.")
+
+
 def setup(bot):
 	bot.add_cog(Core())
