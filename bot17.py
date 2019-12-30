ec5256b21dae56ba8501cd4d28dae870c45fe655
bjrk99 <37029865+bjrk99@users.noreply.github.com>
2019-12-30 06:38:54 +0000

    Missed command_prefix arg

--- bot17.py
+++ bot17.py
@@ -3,13 +3,12 @@ from datetime import datetime
 from discord.ext import commands
 import discord
 
-
 extensions = ("core", "music")
 
 
 class Bot17(commands.AutoShardedBot):
 	def __init__(self, pool=None, *args, **kwargs):
-		super().__init__(*args, **kwargs)
+		super().__init__(self.callable_prefix, *args, **kwargs)
 
 		self.epoch = datetime.now()
 		self.pool = pool
