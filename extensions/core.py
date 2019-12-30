3250a072e8678c2995d2bede2250a5b12d774455
bjrk99 <37029865+bjrk99@users.noreply.github.com>
2019-12-30 06:17:15 +0000

    Add eval command, credit to http://github.com/rapptz

--- extensions/core.py
+++ extensions/core.py
@@ -1,7 +1,55 @@
+import io
+from contextlib import redirect_stdout
+import textwrap
+import traceback
+
 from discord.ext import commands
 
 
 class Core(commands.Cog):
+	def cleanup_code(self, code):
+		if code.startswith("```") and code.endswith("```"):
+			return "\n".join(code.split("\n")[1:-1])
+
+		return code.strip("` \n")
+
+	@commands.command(name="eval")
+	async def cmd_eval(self, ctx, *, code):
+		"""Evaluate some code in Bot17's environment
+		
+		Credit to https://github.com/rapptz
+		"""
+		env = {
+			"ctx": ctx
+		}
+		env.update(globals())
+
+		code = self.cleanup_code(code)
+		stdout = io.StringIO()
+
+		to_compile = f"async def func():\n{textwrap.indent(code, '  ')}"
+
+		try:
+			exec(to_compile, env)
+		except Exception as e:
+			return await ctx.send(f"```py\n{e.__class__.__name__}: {e}\n```")
+
+		func = env["func"]
+		try:
+			with redirect_stdout(stdout):
+				ret = await func()
+		except Exception as e:
+			value = stdout.getvalue()
+			await ctx.send(f"```py\n{value}{traceback.format_exc()}\n```")
+		else:
+			value = stdout.getvalue()
+
+			if ret is None:
+				if value:
+					await ctx.send(f"```py\n{value}\n```")
+			else:
+				await ctx.send(f"```py\n{value}{ret}\n```")
+
 	@commands.command()
 	async def logout(self, ctx):
 		await ctx.bot.pool.close()
