4ec57c25614fc6a8c843f2afdc3a13f273cab2a2
BuildTools <37029865+bjrk99@users.noreply.github.com>
2020-11-20 19:01:02 +0000

    Stole sql command

--- extensions/core.py
+++ extensions/core.py
@@ -1,10 +1,11 @@
 import io
 from contextlib import redirect_stdout
 import textwrap
+import time
 import traceback
 
 from discord.ext import commands
-
+import discord
 
 class Admin(commands.Cog):
 	def cleanup_code(self, code):
@@ -66,6 +67,40 @@ class Admin(commands.Cog):
 		except:
 			await ctx.send("Error loading extension.")
 
+	@commands.command(hidden=True)
+	async def sql(self, ctx, *, query: str):
+		"""Run some SQL
+
+		Credit to https://github.com/rapptz"""
+		query = self.cleanup_code(query)
+
+		is_multistatement = query.count(';') > 1
+		strategy = ctx.bot.pool.execute if is_multistatement else ctx.bot.pool.fetch
+
+		try:
+			start = time.perf_counter()
+			results = await strategy(query)
+			dt = (time.perf_counter() - start) * 1000.0
+		except Exception:
+			return await ctx.send(f'```py\n{traceback.format_exc()}\n```')
+
+		rows = len(results)
+		if is_multistatement or rows == 0:
+			return await ctx.send(f'`{dt:.2f}ms: {results}`')
+
+		headers = list(results[0].keys())
+		table = TabularData()
+		table.set_columns(headers)
+		table.add_rows(list(r.values()) for r in results)
+		render = table.render()
+
+		fmt = f'```\n{render}\n```\n*Returned {plural(rows):row} in {dt:.2f}ms*'
+		if len(fmt) > 2000:
+			fp = io.BytesIO(fmt.encode('utf-8'))
+			await ctx.send('Too many results...', file=discord.File(fp, 'results.txt'))
+		else:
+			await ctx.send(fmt)
+
 
 class Core(commands.Cog):
 	@commands.command(name="ping")
@@ -106,6 +141,76 @@ class Core(commands.Cog):
 
 		await ctx.send("Prefixes removed.")
 		config["updated"] = True
+
+
+class plural:
+	"""Support for SQL command
+
+	Credit to https://github.com/rapptz"""
+	def __init__(self, value):
+		self.value = value
+	def __format__(self, format_spec):
+		v = self.value
+		singular, sep, plural = format_spec.partition('|')
+		plural = plural or f'{singular}s'
+		if abs(v) != 1:
+			return f'{v} {plural}'
+		return f'{v} {singular}'
+
+
+class TabularData:
+	"""Support for SQL command
+
+	Credit to https://github.com/rapptz"""
+	def __init__(self):
+		self._widths = []
+		self._columns = []
+		self._rows = []
+
+	def set_columns(self, columns):
+		self._columns = columns
+		self._widths = [len(c) + 2 for c in columns]
+
+	def add_row(self, row):
+		rows = [str(r) for r in row]
+		self._rows.append(rows)
+		for index, element in enumerate(rows):
+			width = len(element) + 2
+			if width > self._widths[index]:
+				self._widths[index] = width
+
+	def add_rows(self, rows):
+		for row in rows:
+			self.add_row(row)
+
+	def render(self):
+		"""Renders a table in rST format.
+		Example:
+		+-------+-----+
+		| Name  | Age |
+		+-------+-----+
+		| Alice | 24  |
+		|  Bob  | 19  |
+		+-------+-----+
+		"""
+
+		sep = '+'.join('-' * w for w in self._widths)
+		sep = f'+{sep}+'
+
+		to_draw = [sep]
+
+		def get_entry(d):
+			elem = '|'.join(f'{e:^{self._widths[i]}}' for i, e in enumerate(d))
+			return f'|{elem}|'
+
+		to_draw.append(get_entry(self._columns))
+		to_draw.append(sep)
+
+		for row in self._rows:
+			to_draw.append(get_entry(row))
+
+		to_draw.append(sep)
+		return '\n'.join(to_draw)
 
 
 def setup(bot):
